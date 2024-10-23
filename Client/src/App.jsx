2fe5fc5885bc0a31c74117260dc74eeb9b740269
lalit <lalitmohitfacts@gmail.com>
2024-10-23 18:12:05 +0530

    Bug Solved

--- Client/src/App.jsx
+++ Client/src/App.jsx
@@ -1,35 +1,23 @@
-import { Routes, Route } from 'react-router-dom'
+import { Routes, Route, Navigate } from 'react-router-dom';
 import { SignedIn, SignedOut, SignInButton, UserButton } from "@clerk/clerk-react";
-import SignUp from './Pages/SignUp.jsx'
-import Home from './Home.jsx'
-import Login from './Pages/Login.jsx'
-import Dashboard from './Pages/Dashboard.jsx'
-import './index.css'
-import NotFoundPage  from './Pages/NotFoundPage.jsx';
-
-
-
+import SignUp from './Pages/SignUp.jsx';
+import Home from './Home.jsx';
+import Login from './Pages/Login.jsx';
+import Dashboard from './Pages/Dashboard.jsx';
+import './index.css';
+import NotFoundPage from './Pages/NotFoundPage.jsx';
 
 function App() {
   return (
-    <>
-      {/* <header>
-        <SignedOut>
-          <SignInButton />
-        </SignedOut>
-        <SignedIn>
-          <UserButton />
-        </SignedIn>
-      </header> */}
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/SignUp" element={<SignUp />} />
-        <Route path="/login" element={<Login/>} />
-        <Route path="/dashboard" element={<Dashboard/>} />
-        <Route path="/*" element={<NotFoundPage />} />
-      </Routes>
-    </>
-  )
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/Signup" element={<SignUp />} />
+      <Route path="/login" element={<Login />} />
+      <Route path="/dashboard" element={<Dashboard />} />
+      <Route path="/404" element={<NotFoundPage />} />
+      <Route path="*" element={<Navigate replace to="/404" />} />
+    </Routes>
+  );
 }
 
-export default App  
+export default App;
