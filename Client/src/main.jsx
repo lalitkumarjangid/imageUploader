95e0579525dafe643bda048e73ff0a3e15fe8eb1
lalit <lalitmohitfacts@gmail.com>
2024-10-23 18:25:54 +0530

    vite.config.js

--- Client/src/main.jsx
+++ Client/src/main.jsx
@@ -12,9 +12,8 @@ if (!PUBLISHABLE_KEY) {
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <BrowserRouter>
-      <ClerkProvider publishableKey={PUBLISHABLE_KEY} afterSignOutUrl="/">
-        <App />
-      </ClerkProvider>
+      <App />
+
     </BrowserRouter>
   </StrictMode>
 )
--- Client/vite.config.js
+++ Client/vite.config.js
@@ -1,10 +1,17 @@
 // vite.config.js
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+import path from 'path'
 
+/**
+ * @type {import('vite').UserConfig}
+ */
 export default defineConfig({
   plugins: [react()],
-  build: {
-    outDir: 'dist'
-  }
-});
+  resolve: {
+    alias: {
+      // Using __dirname requires importing 'path' and 'url' in ESM
+      '@': path.resolve(path.dirname(new URL(import.meta.url).pathname), './src'),
+    },
+  },
+})
